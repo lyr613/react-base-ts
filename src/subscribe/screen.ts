0ebfae1fa1ed7ac5fa57c6044072aa34d5aeead5
qsoft <qiusoft613@gmail.com>
2019-05-26 07:49:04 +0800

    SAVE

--- src/subscribe/screen.ts
+++ src/subscribe/screen.ts
@@ -1,5 +1,16 @@
-import { BehaviorSubject, fromEvent } from 'rxjs'
-import { throttleTime, mapTo, map } from 'rxjs/operators'
+import {BehaviorSubject, fromEvent} from 'rxjs'
+import {throttleTime, mapTo, map, debounceTime} from 'rxjs/operators'
+
+interface t {
+	/** 屏幕可用宽度 */
+	W: number
+	/** 屏幕可用高度 */
+	H: number
+	/** 竖屏还是横屏 */
+	screen_type: 'pc' | 'm'
+	/** 2: 1366及以下, 3: 1920及以下 */
+	screen_level: number
+}
 
 /** 获得屏幕类型 */
 function get_screen() {
@@ -8,12 +19,13 @@ function get_screen() {
 	const level = [0, 1000, 1361, 1921, 4000].findIndex(v => W < v)
 	return {
 		type,
+		/** 2:1366 3:1920 */
 		level,
 	}
 }
 
 /** 获取屏幕尺寸信息 */
-function get_it() {
+function get_it(): t {
 	const [W, H] = [window.innerWidth, window.innerHeight]
 	const screen = get_screen()
 	return {
@@ -25,13 +37,16 @@ function get_it() {
 }
 
 /** 屏幕尺寸信息
- * @returns screen 数值越大, 屏幕越宽
+ * @returns W 屏幕可用宽度
+ * @returns H 屏幕可用高度
+ * @returns screen_level  2: 1366及以下, 3: 1920及以下
+ * @returns screen_type 竖屏还是横屏
  */
 export const Screen$ = new BehaviorSubject(get_it())
 
 fromEvent(window, 'resize')
 	.pipe(
-		throttleTime(300),
+		debounceTime(300),
 		map(() => get_it()),
 	)
 	.subscribe(Screen$)
