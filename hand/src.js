2e4088a902b56115fb21fa752a66395b6b3fec48
qsoft <qiusoft613@gmail.com>
2019-05-31 20:11:58 +0800

    test

--- hand/src.js
+++ hand/src.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 
 // 适用react@16.8.6
 
+/** webpack识别@ 引用 */
 function handWebpack() {
 	const webpackPath = path.resolve('./', 'node_modules', 'react-scripts', 'config', 'webpack.config.js')
 	console.log(webpackPath)
@@ -21,7 +22,7 @@ function handWebpack() {
 	fs.writeFileSync(webpackPath, nolint)
 	console.log('webpack替换完成')
 }
-
+/** tsconfig @ 引用 */
 function handTsconfig() {
 	const p = path.resolve('./', 'node_modules', 'react-scripts', 'scripts', 'utils', 'verifyTypeScriptSetup.js')
 	console.log(p)
@@ -32,5 +33,25 @@ function handTsconfig() {
 	fs.writeFileSync(p, inset)
 	console.log('tsconfig替换完成')
 }
+
+/** 让jest识别@ */
+function handJest() {
+	const p = path.resolve('./', 'node_modules', 'react-scripts', 'scripts', 'utils', 'createJestConfig.js')
+	const old = fs.readFileSync(p).toString()
+	const arr = old.split(/\n/)
+	const need = arr.every(v => !v.match('@/(.*)'))
+	if (!need) {
+		return
+	}
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].match('moduleNameMapper')) {
+			arr.splice(i + 1, 0, `'^@/(.*)$': '<rootDir>/src/$1',`)
+			break
+		}
+	}
+	const re = arr.join('\n')
+	fs.writeFileSync(p, re)
+}
 handWebpack()
 handTsconfig()
+handJest()
--- src/subscribe/socket.ts
+++ src/subscribe/socket.ts
@@ -1,14 +1,14 @@
-import { webSocket, WebSocketSubject } from 'rxjs/webSocket'
-import { WsBaseurl } from '@/const'
-import { interval, timer, of } from 'rxjs'
-import { filter, catchError } from 'rxjs/operators'
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket'
+import {WsBaseurl} from '@/const'
+import {interval, timer, of} from 'rxjs'
+import {filter, catchError} from 'rxjs/operators'
 
 class obj {
 	[k: string]: any
 }
 
 const subj = webSocket({
-	url: WsBaseurl + 'sid-A',
+	url: 'ws://localhost:9000/sss1',
 	deserializer(v): obj {
 		const d = v.data
 		if (typeof d === 'string') {
@@ -19,13 +19,13 @@ const subj = webSocket({
 })
 const Socket$ = subj.pipe()
 
-timer(1000, 10000).subscribe(() => {
-	subj.next({ heart: '2' })
+timer(1000, 1000).subscribe(() => {
+	subj.next({type: 'heart'})
 })
 
 // const ob1 = Socket$.subscribe(v => {
 //     console.log(v)
 // })
-// const ob2 = Socket$.subscribe(v => {
-//     console.log(v.kkk)
-// })
+const ob2 = Socket$.subscribe(v => {
+	console.log(v)
+})
