37e7930ca7de4714fdf9c491f7e5aaa6f9ccef52
qsoft <qiusoft613@gmail.com>
2019-06-09 21:00:18 +0800

    save

--- hand/src.js
+++ hand/src.js
@@ -5,21 +5,18 @@ const fs = require('fs')
 
 /** webpack识别@ 引用 */
 function handWebpack() {
-	const webpackPath = path.resolve('./', 'node_modules', 'react-scripts', 'config', 'webpack.config.js')
-	console.log(webpackPath)
-	const webpackFs = fs.readFileSync(webpackPath).toString()
-	const withAt = webpackFs.replace(
-		/alias.*[\s\w:.'-@()/]*}/,
-		`
-        alias: {
-            // Support React Native Web
-            // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-            'react-native': 'react-native-web',
-            '@': path.resolve('./', 'src')
-        } `,
-	)
-	const nolint = withAt.replace('include: paths.appSrc', 'include: []')
-	fs.writeFileSync(webpackPath, nolint)
+	const p = path.resolve('./', 'node_modules', 'react-scripts', 'config', 'webpack.config.js')
+	console.log(p)
+	const old = fs.readFileSync(p).toString()
+	const arr = old.split(/\n/)
+	const need = arr.every(v => !v.match(/'@'/))
+	if (need) {
+		const i = arr.findIndex(v => v.match('alias:'))
+		arr.splice(i + 1, 0, `'@': path.resolve('./', 'src'),`)
+	}
+	const re = arr.join('\n')
+	fs.writeFileSync(p, re)
+
 	console.log('webpack替换完成')
 }
 /** tsconfig @ 引用 */
@@ -27,10 +24,25 @@ function handTsconfig() {
 	const p = path.resolve('./', 'node_modules', 'react-scripts', 'scripts', 'utils', 'verifyTypeScriptSetup.js')
 	console.log(p)
 	const old = fs.readFileSync(p).toString()
-	const match = old.match(/paths:.*/)[0]
-	const nw = match.replace('undefined', `{'@/*': ['./*']}`)
-	const inset = old.replace(match, nw)
-	fs.writeFileSync(p, inset)
+	const arr = old.split(/\n/)
+	function baseurl() {
+		const i = arr.findIndex(v => v.match('baseUrl'))
+		const i2 = arr.slice(i, -1).findIndex(v => v.match('}')) + i
+		const ifind = arr.slice(i, i2).findIndex(v => v.match('undefined')) + i
+		if (ifind - i > 0) {
+			arr.splice(ifind, 1, `value: 'src',`)
+		}
+	}
+	function paths() {
+		const i = arr.findIndex(v => v.match('paths:'))
+		if (i > 0) {
+			arr.splice(i, 1, `paths: { value: {'@/*': ['./*']}, reason: '绝对引用添加好了' },`)
+		}
+	}
+	baseurl()
+	paths()
+	const re = arr.join('\n')
+	fs.writeFileSync(p, re)
 	console.log('tsconfig替换完成')
 }
 
@@ -40,18 +52,34 @@ function handJest() {
 	const old = fs.readFileSync(p).toString()
 	const arr = old.split(/\n/)
 	const need = arr.every(v => !v.match('@/(.*)'))
-	if (!need) {
-		return
+	if (need) {
+		const i = arr.findIndex(v => v.match('moduleNameMapper'))
+		arr.splice(i + 1, 0, `'^@/(.*)$': '<rootDir>/src/$1',`)
 	}
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i].match('moduleNameMapper')) {
-			arr.splice(i + 1, 0, `'^@/(.*)$': '<rootDir>/src/$1',`)
-			break
+	const re = arr.join('\n')
+	fs.writeFileSync(p, re)
+}
+
+/** tslint  */
+function handTslint() {
+	const p = path.resolve('./', 'node_modules', 'eslint-config-react-app', 'index.js')
+	const old = fs.readFileSync(p).toString()
+	const arr = old.split(/\n/)
+	// 关闭检查未使用变量
+	function noUse() {
+		const i = arr.findIndex(v => v.match('@typescript-eslint/no-unused-vars'))
+		if (i > 0) {
+			const i2 = i + arr.slice(i).findIndex(v => v.match(']')) + 1
+			arr.slice(i, i2).forEach((v, j) => {
+				arr[i + j] = '// ' + v
+			})
 		}
 	}
+	noUse()
 	const re = arr.join('\n')
 	fs.writeFileSync(p, re)
 }
 handWebpack()
 handTsconfig()
 handJest()
+handTslint()
