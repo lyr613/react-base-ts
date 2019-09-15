c0ddbc6ab456fd324cc0c73d6709926505756dc9
qsoft <qiusoft613@gmail.com>
2019-09-15 18:55:46 +0800

    rej: 重写了屏幕变化观测者

--- src/subscribe/screen.ts
+++ src/subscribe/screen.ts
@@ -1,54 +1,27 @@
-import { BehaviorSubject, fromEvent, timer } from 'rxjs'
-import { throttleTime, mapTo, map, debounceTime, pluck, tap, switchMap, take } from 'rxjs/operators'
+import { BehaviorSubject, fromEvent, timer, Observable } from 'rxjs'
+import { throttleTime, mapTo, map, debounceTime, pluck, tap, switchMap, take, merge, scan } from 'rxjs/operators'
 
-interface t {
-	/** 屏幕可用宽度 */
-	W: number
-	/** 屏幕可用高度 */
-	H: number
-	/** 竖屏还是横屏 */
-	screen_type: 'pc' | 'm'
-	/** 2: 1366及以下, 3: 1920及以下 */
-	screen_level: number
-}
-
-/** 获得屏幕类型 */
-function get_screen() {
-	const [W, H] = [window.innerWidth, window.innerHeight]
-	const type: 'pc' | 'm' = W > H ? 'pc' : 'm'
-	const level = [0, 1000, 1361, 1921, 4000].findIndex(v => W < v)
-	return {
-		type,
-		/** 2:1366 3:1920 */
-		level,
-	}
-}
-
-/** 获取屏幕尺寸信息 */
-function get_it(): t {
-	const [W, H] = [window.innerWidth, window.innerHeight]
-	const screen = get_screen()
-	// console.log(H)
-
-	return {
-		W,
-		H,
-		screen_type: screen.type,
-		screen_level: screen.level,
-	}
-}
-
-/** 屏幕尺寸信息
- * @returns W 屏幕可用宽度
- * @returns H 屏幕可用高度
- * @returns screen_level  2: 1366及以下, 3: 1920及以下
- * @returns screen_type 竖屏还是横屏
+/**
+ * 屏幕尺寸信息
+ * [width, height]
  */
-export const ScreenSize$ = new BehaviorSubject(get_it())
+export const ScreenSize$: Observable<[number, number]> = fromEvent(window, 'resize').pipe(
+    merge(fromEvent(window, 'load')),
+    debounceTime(500),
+    map(() => {
+        return [window.innerWidth, window.innerHeight]
+    }),
+)
 
-fromEvent(window, 'resize')
-	.pipe(
-		debounceTime(300),
-		map(() => get_it()),
-	)
-	.subscribe(ScreenSize$)
+/**
+ * 屏幕滚动信息
+ * [滚动方向: 负数向上正数向下, 现在的顶部距离]
+ */
+export const ScreenScroll$: Observable<[number, number]> = fromEvent(window, 'scroll').pipe(
+    map(() => [0, document.documentElement.scrollTop || document.body.scrollTop] as [number, number]),
+    scan((p, v) => {
+        const [_, nt] = v
+        const [a, pt] = p
+        return [nt - pt, nt]
+    }),
+)
