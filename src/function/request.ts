ef8c0dddba65961798073ca82febed754916f5bb
qsoft <qiusoft613@gmail.com>
2019-09-15 13:06:37 +0800

    rej: 完善请求函数获取完整路径的方式

--- src/function/request.ts
+++ src/function/request.ts
@@ -5,19 +5,26 @@ import { of } from 'rxjs'
 
 type method = 'post' | 'get' | 'put' | 'delete'
 type body = {
-	[k: string]: any
+    [k: string]: any
+}
+
+function get_url(s: string) {
+    if (s.match(/^http/)) {
+        return s
+    }
+    return BaseUrl + s
 }
 export function RxAjax(url: string, method: method = 'post', body?: body) {
-	return ajax({
-		url: BaseUrl + url,
-		method,
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: localStorage.getItem('token'),
-		},
-		body,
-	}).pipe(
-		map(v => v.response),
-		catchError(err => of(err.response)),
-	)
+    return ajax({
+        url: get_url(url),
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: localStorage.getItem('token'),
+        },
+        body,
+    }).pipe(
+        map((v) => v.response),
+        catchError((err) => of(err.response)),
+    )
 }
