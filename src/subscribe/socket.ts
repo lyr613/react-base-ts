d231b3ac921d6803240944a9b15d9b65e2083864
qsoft <qiusoft613@gmail.com>
2019-09-15 18:56:34 +0800

    rej: 默认不尝试连接socket

--- src/subscribe/socket.ts
+++ src/subscribe/socket.ts
@@ -3,10 +3,10 @@ import { BaseWsUrl } from '@/const'
 import { interval, timer, of, Subject, ReplaySubject } from 'rxjs'
 import { filter, catchError, tap, map, switchMap, mapTo, exhaustMap, switchMapTo } from 'rxjs/operators'
 class obj {
-	[k: string]: any
+    [k: string]: any
 }
 class SocketDto {
-	[k: string]: any
+    [k: string]: any
 }
 
 /** 上传使用此subj */
@@ -14,61 +14,61 @@ export const SocketSend$ = new ReplaySubject<WebSocketSubject<obj>>(1)
 
 /** 每20s自动重试连接的socket */
 const subj$ = timer(0, 1000 * 20).pipe(
-	map(() => (Math.random() * 999999) | 0),
-	exhaustMap(s => {
-		const ws = webSocket({
-			url: BaseWsUrl + s,
-			deserializer(v): obj {
-				if (v == null) {
-					return {}
-				}
-				try {
-					return JSON.parse(v.data)
-				} catch (error) {
-					return {}
-				}
-			},
-		})
-		ws.subscribe(undefined, err => {
-			console.log('socket断开连接')
-			ws.complete()
-		})
-		SocketSend$.next(ws)
-		return ws.pipe(catchError(err => of(err)))
-	}),
+    map(() => (Math.random() * 999999) | 0),
+    exhaustMap((s) => {
+        const ws = webSocket({
+            url: BaseWsUrl + s,
+            deserializer(v): obj {
+                if (v == null) {
+                    return {}
+                }
+                try {
+                    return JSON.parse(v.data)
+                } catch (error) {
+                    return {}
+                }
+            },
+        })
+        ws.subscribe(undefined, (err) => {
+            console.log('socket断开连接')
+            ws.complete()
+        })
+        SocketSend$.next(ws)
+        return ws.pipe(catchError((err) => of(err)))
+    }),
 )
 
 /** 接收使用此subj */
 export const Socket$ = new Subject<SocketDto>()
 subj$
-	.pipe(
-		filter(v => {
-			if (typeof v !== 'object') {
-				return false
-			}
-			if (Object.entries(v).length === 0) {
-				return false
-			}
-			if (v.memberTrend === undefined) {
-				return false
-			}
-			return true
-		}),
-		tap(v => {
-			// console.log('socket传来消息')
-			// console.log(v)
-		}),
-	)
-	.subscribe(Socket$)
+    .pipe(
+        filter((v) => {
+            if (typeof v !== 'object') {
+                return false
+            }
+            if (Object.entries(v).length === 0) {
+                return false
+            }
+            if (v.memberTrend === undefined) {
+                return false
+            }
+            return true
+        }),
+        tap((v) => {
+            // console.log('socket传来消息')
+            // console.log(v)
+        }),
+    )
+    .subscribe(Socket$)
 
 // 心跳
-timer(1000, 1000 * 60 * 5)
-	.pipe(
-		switchMap(() => SocketSend$),
-		filter(v => !!v),
-	)
-	.subscribe(subj => {
-		subj.next({
-			heart: 2,
-		})
-	})
+// timer(1000, 1000 * 60 * 5)
+// 	.pipe(
+// 		switchMap(() => SocketSend$),
+// 		filter(v => !!v),
+// 	)
+// 	.subscribe(subj => {
+// 		subj.next({
+// 			heart: 2,
+// 		})
+// 	})
