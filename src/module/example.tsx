92a9b59cf0a451e443dee33187b71dcc5ec12141
qsoft <qiusoft613@gmail.com>
2019-11-06 11:00:46 +0800

    doc

--- src/module/example.tsx
+++ src/module/example.tsx
@@ -2,7 +2,9 @@
 import React, { useState, useEffect } from 'react'
 import s from './s.module.scss'
 
+/** 示例组件 */
 export default function Example() {
+    /** 示例数组 */
     const arr = Array.from(
         {
             length: 12,
@@ -11,6 +13,7 @@ export default function Example() {
             return `第${i + 1}个`
         },
     )
+    // 被选择的下标
     const [did_select, set_did_select] = useState(-1)
     useEffect(() => {
         // 相当于 vue mounted + watch
@@ -34,10 +37,13 @@ export default function Example() {
         </div>
     )
 }
-
 interface pp {
     s: string
 }
+/**
+ * 示例组件
+ * @param p.s 传入显示字符串
+ */
 function Comp(p: pp) {
     const str = p.s + '...'
     return <div className={s.Comp}>{str}</div>
