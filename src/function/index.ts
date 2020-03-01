24d2efb3493d09292582a40cf4c8fcbaef6eeffb
qsoft <qiusoft613@gmail.com>
2020-03-01 18:11:12 +0800

    clean

--- src/function/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './log'
-export * from './rxajax'
-export * from './time-util'
--- src/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-interface Window {
-    THREE: any
-}
-
-interface Storage {}
-
-type Param = { [k: string]: any }
