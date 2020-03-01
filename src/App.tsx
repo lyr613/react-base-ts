136c9807bad7f1ea1dc4b36f91b7adb87ab92d84
qsoft <qiusoft613@gmail.com>
2020-03-01 18:54:33 +0800

    chore: 类型声明

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import TTT from './arena/t'
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <div className="App">
+            <TTT />
+        </div>
+    )
 }
 
 export default App
---
+++ src/arena/t.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+import React, { useState, useEffect } from 'react'
+import s from './s.module.scss'
+
+export default function TTT() {
+    return <div className={s.TTT}>{23333}</div>
+}
