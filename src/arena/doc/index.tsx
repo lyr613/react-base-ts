7c55f83c7fd6f809cbe8479c2fabde6970ab00cb
qsoft <qiusoft613@gmail.com>
2020-08-01 12:27:26 +0800

    chore: 升级依赖包

--- src/arena/doc/index.tsx
+++ src/arena/doc/index.tsx
@@ -6,10 +6,6 @@ import { rxajax, mkurl } from '@/util/request'
 
 /** tsdoc 示例 */
 export default function Doc() {
-    useEffect(() => {
-        rxajax(mkurl('services/smartpark-community/api/shopsControl/getIndustries'), 'get').subscribe((re) => {
-            console.log(re)
-        })
-    }, [])
-    return <div className={s.Doc}></div>
+    useEffect(() => {}, [])
+    return <div className={s.Doc}>23333</div>
 }
