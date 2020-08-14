d74a216a74b578fe5d05cbd07ce9fa9ec9b0e263
qsoft <qiusoft613@gmail.com>
2020-08-14 15:54:09 +0800

    feat: 一个拖拽排序的例子

--- src/arena/home/style.ts
+++ src/arena/home/style.ts
@@ -3,10 +3,15 @@ import { StyleSheet } from 'aphrodite'
 interface style {
     /** 组件顶层 */
     root: object
+    /** 一项 */
+    one: object
 }
 
 export const style: style = StyleSheet.create<style>({
     root: {
         fontSize: '24px',
+    },
+    one: {
+        cursor: '',
     },
 })
