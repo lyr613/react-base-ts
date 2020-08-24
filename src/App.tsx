d13cb1df4b84008f4e19d074833a55d3bce53b5a
qsoft <qiusoft613@gmail.com>
2020-08-24 12:03:29 +0800

    重置react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Route, HashRouter, useHistory, Switch } from 'react-router-dom'
-import Home from './arena/home'
-import CanvasDraw from './arena/canvas'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <HashRouter>
-                <Switch>
-                    <Route path="/home" component={Home} />
-                    <Route path="/canvas" component={CanvasDraw} />
-                    <Route component={Empty} />
-                </Switch>
-            </HashRouter>
-        </div>
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
-
-function Empty() {
-    const p = useHistory()
-    p.push('home')
-    return null
-}
+export default App;
