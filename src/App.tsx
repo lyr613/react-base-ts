fd33880e12f2097f3d42aa35521ea91378b510ab
qsoft <qiusoft613@gmail.com>
2020-11-10 17:27:22 +0800

    del: 无用代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Some from 'arena/ttt'
-import { timer } from 'rxjs'
 import { HashRouter, Route, Switch, useHistory } from 'react-router-dom'
 import { router_pusher$ } from 'routers/pusher'
 
 function App() {
-    const [a, next_a] = useState(0)
-    useEffect(() => {
-        timer(0, 1000).subscribe(next_a)
-    }, [])
     return (
         <div className="App">
             <HashRouter>
